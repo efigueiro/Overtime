6fe376465857490f33f8e639edbf66cb7ae6e232
efigueiro <everson.figueiro@gmail.com>
2013-08-22 00:47:44 -0300

    Ajustes basicos

--- src/com/overtime/model/entity/User.java
+++ src/com/overtime/model/entity/User.java
@@ -3,6 +3,11 @@ package com.overtime.model.entity;
 import java.io.Serializable;
 
 public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	
 	
 	
